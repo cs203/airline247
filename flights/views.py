bd2b2254a75d26a010ce57a73fa710148725c824
cs203 <67407203+cs203@users.noreply.github.com>
2025-02-19 13:59:20 +0700

    users start

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,18 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     passengers = flight.passengers.all()
-    return render(request, "flights/flight.html", {"flight": flight, "passengers": passengers})
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+    return render(request, "flights/flight.html", {"flight": flight,
+                                                   "passengers": passengers,
+                                                   "non_passengers": non_passengers
+                                                   }
+                )
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(id=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(id=passenger_id)
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
